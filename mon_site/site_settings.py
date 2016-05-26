6f2ff48fd6dfc4404b0b3091c093cbb31932dd63
Quentin48 <quentin.garrido@icloud.com>
2016-05-26 12:10:29 +0200

    Delete site_settings.py

--- mon_site/site_settings.py
+++
@@ -1,9 +0,0 @@
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.mysql',
-        'NAME': 'dojoagile',
-        'USER': 'root',
-        'PASSWORD': 'azerty',
-        'HOTE' : 'localhost'
-    }
-}
